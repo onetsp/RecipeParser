ee0599ee3583bd592fe0796271d8cd4ae868dbb4
Mike Brittain <mike@mikebrittain.com>
2013-09-10 19:02:43 -0400

    Recipe parser for Elanaspantry.com

---
+++ lib/RecipeParser/Parser/Elanaspantrycom.php
@@ -0,0 +1,84 @@
+<?php
+
+class RecipeParser_Parser_Elanaspantrycom {
+
+    public function parse($html, $url) {
+        $recipe = RecipeParser_Parser_Microformat::parse($html, $url);
+
+        libxml_use_internal_errors(true);
+        $html = mb_convert_encoding($html, 'HTML-ENTITIES', "UTF-8");
+        $doc = new DOMDocument();
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $html);
+        $xpath = new DOMXPath($doc);
+
+        if (!$recipe->title) {
+            $nodes = $xpath->query('//div[@class="box"]/strong');
+            if ($nodes->length) {
+                $line = $nodes->item(0)->nodeValue;
+                $line = RecipeParser_Text::formatTitle($line);
+                $recipe->title = $line;
+            }
+        }
+
+        if (!$recipe->yield) {
+            $nodes = $xpath->query('//div[@class="box"]/div');
+            foreach ($nodes as $node) {
+                $line = trim($node->nodeValue);
+                if (stripos($line, "makes") === 0) {
+                    $line = RecipeParser_Text::formatYield($line);
+                    $recipe->yield = $line;
+                    break;
+                }
+            }
+        }
+
+        if (!count($recipe->ingredients)) {
+            $nodes = $xpath->query('//div[@class="box"]');
+            if ($nodes->length) {
+                $nodes = $nodes->item(0)->childNodes;
+
+                $str = "";
+                foreach ($nodes as $node) {
+                    if (in_array($node->nodeName, array("#text", "a", "br"))) {
+                        if ($node->nodeName == "br") {
+                            $str .= "<br>";
+                        } else {
+                            $line = $node->nodeValue;
+                            $str .= $line;
+                        }
+                    }
+                }
+                $lines = explode("<br>", $str);
+                foreach ($lines as $line) {
+                    $line = RecipeParser_Text::formatAsOneLine($line);
+                    $recipe->appendIngredient($line);
+                }
+            }
+        }
+
+        if (!count($recipe->instructions)) {
+            $nodes = $xpath->query('//div[@class="box"]/ol/li');
+            foreach ($nodes as $node) {
+                $line = $node->nodeValue;
+                $line = RecipeParser_Text::formatAsOneLine($line);
+                $recipe->appendInstruction($line);
+            }
+        }
+
+        if (!$recipe->photo_url) {
+            $nodes = $xpath->query('//meta[@property="og:image"]');
+            foreach ($nodes as $node) {
+                $line = $node->getAttribute("content");
+                if (strpos($line, "wp-content") !== false) {
+                    $recipe->photo_url = $line;
+                    break;
+                }
+            }
+        }
+
+        return $recipe;
+    }
+
+}
+
+?>
---
+++ tests/RecipeParser_Parser_ElanaspantrycomTest.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once "../bootstrap.php";
+
+class RecipeParser_Parser_Elanaspantrycom_Test extends PHPUnit_Framework_TestCase {
+
+    public function test_cranberry_coconut_power_bars() {
+
+        $path = "data/elanaspantry_com_cranberry_coconut_power_bars_paleo_power_bars_curl.html";
+        $url  = "http://www.elanaspantry.com/cranberry-coconut-power-bars/";
+
+        $recipe = RecipeParser::parse(file_get_contents($path), $url);
+        if (isset($_SERVER['VERBOSE'])) print_r($recipe);
+
+        $this->assertEquals('Paleo Power Bars', $recipe->title);
+
+        $this->assertEquals('25 bars', $recipe->yield);
+
+        $this->assertEquals(1, count($recipe->ingredients));
+        $this->assertEquals('', $recipe->ingredients[0]['name']);
+        $this->assertEquals(7, count($recipe->ingredients[0]['list']));
+
+        $this->assertEquals(1, count($recipe->instructions));
+        $this->assertEquals('', $recipe->instructions[0]['name']);
+        $this->assertEquals(5, count($recipe->instructions[0]['list']));
+
+        $this->assertEquals('http://www.elanaspantry.com/blog/wp-content/uploads/2013/08/Cranberry-Coconut-Power-Bars-1876.jpg',
+                            $recipe->photo_url);
+    }
+
+    public function test_coconut_cupcakes_key_lime_icing() {
+
+        $path = "data/elanaspantry_com_gluten_free_and_nut_free_coconut_cupcakes_curl.html";
+        $url  = "http://www.elanaspantry.com/coconut-cupcakes-key-lime-icing/";
+
+        $recipe = RecipeParser::parse(file_get_contents($path), $url);
+        if (isset($_SERVER['VERBOSE'])) print_r($recipe);
+
+        $this->assertEquals('Coconut Cupcakes with Key Lime Icing', $recipe->title);
+        $this->assertEquals('10 cupcakes', $recipe->yield);
+
+        $this->assertEquals(1, count($recipe->ingredients));
+        $this->assertEquals('', $recipe->ingredients[0]['name']);
+        $this->assertEquals(7, count($recipe->ingredients[0]['list']));
+
+        $this->assertEquals("3 eggs", $recipe->ingredients[0]['list'][0]);
+        $this->assertEquals("½ teaspoon baking soda", $recipe->ingredients[0]['list'][5]);
+        $this->assertEquals("½ cup unsweetened shredded coconut", $recipe->ingredients[0]['list'][6]);
+
+        $this->assertEquals(1, count($recipe->instructions));
+        $this->assertEquals('', $recipe->instructions[0]['name']);
+        $this->assertEquals(8, count($recipe->instructions[0]['list']));
+
+        $this->assertEquals('http://www.elanaspantry.com/blog/wp-content/uploads/2010/05/gluten-free-coconut-cupcakes-key-lime-frosting-DSC_5328.jpg',
+                            $recipe->photo_url);
+    }
+
+    public function test_pumpkin_cranberry_upside_down_cake() {
+
+        $path = "data/elanaspantry_com_pumpkin_cranberry_upside_down_cake_gluten_free_dessert_curl.html";
+        $url  = "http://www.elanaspantry.com/pumpkin-cranberry-upside-down-cake/";
+
+        $recipe = RecipeParser::parse(file_get_contents($path), $url);
+        if (isset($_SERVER['VERBOSE'])) print_r($recipe);
+
+        $this->assertEquals('Pumpkin Cranberry Upside-Down Cake', $recipe->title);
+
+        $this->assertEquals('24 servings', $recipe->yield);
+
+        $this->assertEquals(1, count($recipe->ingredients));
+        $this->assertEquals('', $recipe->ingredients[0]['name']);
+        $this->assertEquals(14, count($recipe->ingredients[0]['list']));
+
+        $this->assertEquals(1, count($recipe->instructions));
+        $this->assertEquals('', $recipe->instructions[0]['name']);
+        $this->assertEquals(12, count($recipe->instructions[0]['list']));
+
+        $this->assertEquals('http://www.elanaspantry.com/blog/wp-content/uploads/2007/11/pumpkin_cranberry_upside-down_cake.jpg',
+                            $recipe->photo_url);
+    }
+
+}
