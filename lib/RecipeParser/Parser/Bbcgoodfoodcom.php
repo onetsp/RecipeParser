d25a3628b56fb3fe34335e69dc60416f15f99974
Mike Brittain <mike@mikebrittain.com>
2013-09-19 08:23:03 -0400

    Fixed parser for BBC Good Food.

--- lib/RecipeParser/Parser/Bbcgoodfoodcom.php
+++ lib/RecipeParser/Parser/Bbcgoodfoodcom.php
@@ -3,8 +3,7 @@
 class RecipeParser_Parser_Bbcgoodfoodcom {
 
     public function parse($html, $url) {
-        // Get all of the standard hrecipe stuff we can find.
-        $recipe = RecipeParser_Parser_Microformat::parse($html, $url);
+        $recipe = RecipeParser_Parser_MicrodataSchema::parse($html, $url);
 
         // Turn off libxml errors to prevent mismatched tag warnings.
         libxml_use_internal_errors(true);
@@ -13,59 +12,24 @@ class RecipeParser_Parser_Bbcgoodfoodcom {
         $doc->loadHTML('<?xml encoding="UTF-8">' . $html);
         $xpath = new DOMXPath($doc);
 
-        // Strip some duplicated contents from DOM
-        $nodes = $xpath->query("//div[@id='printSidebar']");
-        foreach ($nodes as $node) {
-            $node->parentNode->removeChild($node);
-        }
-        
-        // Prep / Cook times
-        $nodes = $xpath->query('//div[@id="prep"]/p');
-        foreach ($nodes as $node) {
-            $line = RecipeParser_Text::FormatAsOneLine($node->nodeValue);
-            if (strpos($line, 'Prep') === 0) {
-                $line = substr($line, 5);
-                $recipe->time['prep'] = RecipeParser_Times::toMinutes($line);
-
-            } else if (strpos($line, 'Cook') === 0) {
-                $line = substr($line, 5);
-                $recipe->time['cook'] = RecipeParser_Times::toMinutes($line);
-
-            } else if (strpos($line, 'Ready in') === 0) {
-                $line = substr($line, 9);
-                $recipe->time['total'] = RecipeParser_Times::toMinutes($line);
-            }
-        }
-
         // Ingredients
         $recipe->resetIngredients();           
 
-        $nodes = $xpath->query('//*[@id="ingredients"]/*');
+        $nodes = $xpath->query('//*[@id="recipe-ingredients"]/div/*');
         foreach ($nodes as $node) {
 
-            if ($node->nodeName == 'h4') {
+            if ($node->nodeName == 'h3') {
                 $line = $node->nodeValue;
                 $line = RecipeParser_Text::formatSectionName($line);
                 $recipe->addIngredientsSection($line);
             
             } else if ($node->nodeName == 'ul') {
-                $subnodes = $xpath->query('./li', $node);
-                foreach ($subnodes as $subnode) {
+                foreach ($node->childNodes as $subnode) {
                     $line = $subnode->nodeValue;
                     $line = RecipeParser_Text::formatAsOneLine($line);
-
                     $recipe->appendIngredient($line);
                 }
             }
-        }
-
-        // Instructions
-        $recipe->resetInstructions();
-
-        $nodes = $xpath->query('//div[@id="method"]//li');
-        foreach ($nodes as $node) {
-            $line = RecipeParser_Text::FormatAsOneLine($node->nodeValue);
-            $recipe->appendInstruction($line);
         }
 
         return $recipe;
--- tests/RecipeParser_Parser_BbcgoodfoodcomTest.php
+++ tests/RecipeParser_Parser_BbcgoodfoodcomTest.php
@@ -5,18 +5,18 @@ require_once '../bootstrap.php';
 class RecipeParser_Parser_BbcgoodfoodcomTest extends PHPUnit_Framework_TestCase {
 
     public function test_chicken_herb_rosti() {
-        $path_orig = "data/bbcgoodfood_com_chicken_herb_r_246_sti_topped_pies_s_bbc_curl.html";
+        $path_orig = "data/bbcgoodfood_com_chicken_amp_herb_r_sti_topped_pies_curl.html";
         $url = "http://www.bbcgoodfood.com/recipes/527632/chicken-and-herb-rstitopped-pies";
 
         $recipe = RecipeParser::parse(file_get_contents($path_orig), $url);
         if (isset($_SERVER['VERBOSE'])) print_r($recipe);
 
         $this->assertEquals('Chicken & herb rösti-topped pies', $recipe->title);
-        $this->assertEquals('olive magazine', $recipe->credits);
+        $this->assertEquals('olive', $recipe->credits);
 
         $this->assertEquals(15, $recipe->time['prep']);
-        $this->assertEquals(1, $recipe->time['cook']);  // This is wrong, but it's what is reported in the recipe
-        $this->assertEquals(0, $recipe->time['total']);
+        $this->assertEquals(1, $recipe->time['cook']);
+        $this->assertEquals(75, $recipe->time['total']);
         $this->assertEquals('4 servings', $recipe->yield);
 
         $this->assertEquals(1, count($recipe->ingredients));
@@ -27,12 +27,12 @@ class RecipeParser_Parser_BbcgoodfoodcomTest extends PHPUnit_Framework_TestCase 
         $this->assertEquals('', $recipe->instructions[0]['name']);
         $this->assertEquals(3, count($recipe->instructions[0]['list']));
 
-        $this->assertEquals('http://www.bbcgoodfood.com/recipes/527632/images/527632_MEDIUM.jpg', 
+        $this->assertEquals('http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--283473_12.jpg', 
                             $recipe->photo_url);
     }
 
     public function test_lemon_poppyseed() {
-        $path_orig = "data/bbcgoodfood_com_lemon_poppyseed_cupcakes_s_bbc_good_curl.html";
+        $path_orig = "data/bbcgoodfood_com_lemon_amp_poppyseed_cupcakes_curl.html";
         $url = "http://www.bbcgoodfood.com/recipes/470636/lemon-and-poppyseed-cupcakes";
 
         $recipe = RecipeParser::parse(file_get_contents($path_orig), $url);
@@ -43,8 +43,8 @@ class RecipeParser_Parser_BbcgoodfoodcomTest extends PHPUnit_Framework_TestCase 
 
         $this->assertEquals(40, $recipe->time['prep']);
         $this->assertEquals(22, $recipe->time['cook']);
-        $this->assertEquals(0, $recipe->time['total']);
-        $this->assertEquals('', $recipe->yield);
+        $this->assertEquals(60, $recipe->time['total']);
+        $this->assertEquals('12', $recipe->yield);
 
         $this->assertEquals(2, count($recipe->ingredients));
         $this->assertEquals('', $recipe->ingredients[0]['name']);
@@ -56,12 +56,12 @@ class RecipeParser_Parser_BbcgoodfoodcomTest extends PHPUnit_Framework_TestCase 
         $this->assertEquals('', $recipe->instructions[0]['name']);
         $this->assertEquals(3, count($recipe->instructions[0]['list']));
 
-        $this->assertEquals('http://www.bbcgoodfood.com/recipes/470636/images/470636_MEDIUM.jpg', 
+        $this->assertEquals('http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--1273612_8.jpg',
                             $recipe->photo_url);
     }
 
     public function test_ultimate_chocolate_cake() {
-        $path_orig = "data/bbcgoodfood_com_ultimate_chocolate_cake_s_bbc_good_curl.html";
+        $path_orig = "data/bbcgoodfood_com_ultimate_chocolate_cake_curl.html";
         $url = "http://www.bbcgoodfood.com/recipes/3092/ultimate-chocolate-cake";
 
         $recipe = RecipeParser::parse(file_get_contents($path_orig), $url);
@@ -72,8 +72,8 @@ class RecipeParser_Parser_BbcgoodfoodcomTest extends PHPUnit_Framework_TestCase 
 
         $this->assertEquals(40, $recipe->time['prep']);
         $this->assertEquals(90, $recipe->time['cook']);
-        $this->assertEquals(0, $recipe->time['total']);
-        $this->assertEquals('', $recipe->yield);
+        $this->assertEquals(130, $recipe->time['total']);
+        $this->assertEquals('cuts into 14 slices', $recipe->yield);
 
         $this->assertEquals(2, count($recipe->ingredients));
         $this->assertEquals('', $recipe->ingredients[0]['name']);
@@ -85,18 +85,15 @@ class RecipeParser_Parser_BbcgoodfoodcomTest extends PHPUnit_Framework_TestCase 
         $this->assertEquals('', $recipe->instructions[0]['name']);
         $this->assertEquals(5, count($recipe->instructions[0]['list']));
 
-        $this->assertEquals('http://www.bbcgoodfood.com/recipes/3092/images/3092_MEDIUM.jpg',
+        $this->assertEquals('http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--1043451_11.jpg',
                             $recipe->photo_url);
     }
 
     public function test_slow_cooked_chinese_beef() {
-        $path_orig = "data/bbcgoodfood_com_slow_cooked_chinese_beef_s_bbc_good_curl.html";
+        $path_orig = "data/bbcgoodfood_com_slow_cooked_chinese_beef_curl.html";
         $url = "http://www.bbcgoodfood.com/recipes/96613/slowcooked-chinese-beef";
 
         $recipe = RecipeParser::parse(file_get_contents($path_orig), $url);
-        
-
-
         if (isset($_SERVER['VERBOSE'])) print_r($recipe);
 
         $this->assertEquals('Slow-cooked Chinese beef', $recipe->title);
