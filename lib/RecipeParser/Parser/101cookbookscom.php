892c0f088fcd3517da97732db54603292f275552
Mike Brittain <mike@mikebrittain.com>
2013-10-27 03:44:54 -0400

    New parser for 101 Cookbooks.

---
+++ lib/RecipeParser/Parser/101cookbookscom.php
@@ -0,0 +1,104 @@
+<?php
+
+class RecipeParser_Parser_101cookbookscom {
+
+    public function parse($html, $url) {
+        $recipe = RecipeParser_Parser_Microformat::parse($html, $url);
+
+        // Turn off libxml errors to prevent mismatched tag warnings.
+        libxml_use_internal_errors(true);
+        $html = mb_convert_encoding($html, 'HTML-ENTITIES', "UTF-8");
+        $doc = new DOMDocument();
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $html);
+        $xpath = new DOMXPath($doc);
+
+        // Description
+        $description = "";
+        $nodes = $xpath->query('//div[@id="recipe"]/p/i');
+        foreach ($nodes as $node) {
+            $line = trim($node->nodeValue);
+            if (strpos($line, "Adapted from") === false) {
+                $description .= $line . "\n\n";
+            }
+        }
+        $description = trim($description);
+        $recipe->description = $description;
+
+        // Ingredients
+        $recipe->resetIngredients();
+        $lines = array();
+
+        // Add ingredients to blob
+        $nodes = $xpath->query('//div[@id="recipe"]/blockquote/p');
+        foreach ($nodes as $node) {
+            foreach ($node->childNodes as $child) {
+                $line = trim($child->nodeValue);
+                switch($child->nodeName) {
+                    case "strong":
+                    case "b":
+                        if (strpos($line, ":") === false) {
+                            $line .= ":";
+                        }
+
+                        $lines[] = $line;
+                        break;
+
+                    case "#text":
+                    case "div":
+                    case "p":
+                        $lines[] = $line;
+                        break;
+                }
+            }
+        }
+        foreach ($lines as $line) {
+            if (RecipeParser_Text::matchSectionName($line)) {
+                $recipe->addIngredientsSection(RecipeParser_Text::formatSectionName($line));
+            } else {
+                $line = RecipeParser_Text::formatAsOneLine($line);
+                $recipe->appendIngredient($line);
+            }
+        }
+        
+
+        // Instructions
+        $recipe->resetInstructions();
+        $lines = array();
+
+        $nodes = $xpath->query('//div[@id="recipe"]/*');
+        $passed_ingredients = false;
+        foreach ($nodes as $node) {
+            if ($node->nodeName == "blockquote") {
+                $passed_ingredients = true;
+                continue;
+            }
+            if ($node->nodeName == "p") {
+
+                if ($passed_ingredients) {
+                    $line = trim($node->nodeValue);
+
+                    // Finished with ingredients once we hit "Adapted" notes or any <p> 
+                    // with a class attribute.
+                    if (stripos($line, "Adapted from") !== false) {
+                        break;
+                    } else if ($node->getAttribute("class")) {
+                        break;
+                    }
+
+                    // Servings?
+                    if (stripos($line, "Serves ") === 0) {
+                        $recipe->yield = RecipeParser_Text::formatYield($line);
+                        continue;
+                    }
+
+                    $recipe->appendInstruction(RecipeParser_Text::formatAsOneLine($node->nodeValue));
+                }
+            }
+        }
+
+        return $recipe;
+    }
+
+}
+
+?>
---
+++ tests/RecipeParser_Parser_101cookbookscomTest.php
@@ -0,0 +1,120 @@
+<?php
+
+require_once "../bootstrap.php";
+
+class RecipeParser_Parser_101cookbookscom_Test extends PHPUnit_Framework_TestCase {
+
+    public function test_glissade_chocolate_pudding_recipe() {
+        $path = "data/101cookbooks_com_glissade_chocolate_pudding_cookbooks_curl.html";
+        $url  = "http://www.101cookbooks.com/archives/glissade-chocolate-pudding-recipe.html";
+
+        $recipe = RecipeParser::parse(file_get_contents($path), $url);
+        if (isset($_SERVER['VERBOSE'])) print_r($recipe);
+
+        $this->assertEquals('Glissade Chocolate Pudding', $recipe->title);
+
+        $this->assertEquals(3, $recipe->time['prep']);
+        $this->assertEquals(5, $recipe->time['cook']);
+        $this->assertEquals(0, $recipe->time['total']);
+        $this->assertEquals('2-4 servings', $recipe->yield);
+
+        $this->assertEquals(1, count($recipe->ingredients));
+        $this->assertEquals('', $recipe->ingredients[0]['name']);
+        $this->assertEquals(7, count($recipe->ingredients[0]['list']));
+
+        $this->assertEquals(1, count($recipe->instructions));
+        $this->assertEquals('', $recipe->instructions[0]['name']);
+        $this->assertEquals(3, count($recipe->instructions[0]['list']));
+
+        $this->assertRegExp('/^Use the best quality chocolate/', $recipe->description);
+
+        $this->assertEquals('http://www.101cookbooks.com/mt-static/images/food/slippery_chocolate_pudding_recipe.jpg',
+                            $recipe->photo_url);
+    }
+
+    public function test_lemony_olive_oil_banana_bread_recipe() {
+        $path = "data/101cookbooks_com_lemony_olive_oil_banana_bread_cookbooks_curl.html";
+        $url  = "http://www.101cookbooks.com/archives/lemony-olive-oil-banana-bread-recipe.html";
+
+        $recipe = RecipeParser::parse(file_get_contents($path), $url);
+        if (isset($_SERVER['VERBOSE'])) print_r($recipe);
+
+        $this->assertEquals('Lemony Olive Oil Banana Bread', $recipe->title);
+
+        $this->assertEquals(10, $recipe->time['prep']);
+        $this->assertEquals(50, $recipe->time['cook']);
+        $this->assertEquals(0, $recipe->time['total']);
+
+        // this gets mixed in with instructions.
+        //$this->assertEquals('10 servings', $recipe->yield);
+
+        $this->assertEquals(2, count($recipe->ingredients));
+        $this->assertEquals('', $recipe->ingredients[0]['name']);
+        $this->assertEquals(12, count($recipe->ingredients[0]['list']));
+        $this->assertEquals('Glaze', $recipe->ingredients[1]['name']);
+        $this->assertEquals(3, count($recipe->ingredients[1]['list']));
+
+        $this->assertEquals(1, count($recipe->instructions));
+        $this->assertEquals('', $recipe->instructions[0]['name']);
+        $this->assertEquals(5, count($recipe->instructions[0]['list']));
+
+        $this->assertRegExp('/^Melissa\'s recipe instructs/', $recipe->description);
+
+        $this->assertEquals('http://www.101cookbooks.com/mt-static/images/food/lemon_banana_bread_recipe.jpg',
+                            $recipe->photo_url);
+    }
+
+    public function test_roasted_squash_chile_and_mozzarella_salad_recipe() {
+
+        $path = "data/101cookbooks_com_roasted_squash_chile_and_mozzarella_salad_curl.html";
+        $url  = "http://www.101cookbooks.com/archives/roasted-squash-chile-and-mozzarella-salad-recipe.html";
+
+        $recipe = RecipeParser::parse(file_get_contents($path), $url);
+        if (isset($_SERVER['VERBOSE'])) print_r($recipe);
+
+        $this->assertEquals('Roasted Squash, Chile, and Mozzarella Salad', $recipe->title);
+
+        $this->assertEquals(5, $recipe->time['prep']);
+        $this->assertEquals(25, $recipe->time['cook']);
+        $this->assertEquals(0, $recipe->time['total']);
+        $this->assertEquals('4 servings', $recipe->yield);
+
+        $this->assertEquals(1, count($recipe->ingredients));
+        $this->assertEquals('', $recipe->ingredients[0]['name']);
+        $this->assertEquals(9, count($recipe->ingredients[0]['list']));
+
+        $this->assertEquals(1, count($recipe->instructions));
+        $this->assertEquals('', $recipe->instructions[0]['name']);
+        $this->assertEquals(3, count($recipe->instructions[0]['list']));
+
+        $this->assertEquals('http://www.101cookbooks.com/mt-static/images/food/kinfolk_squash_salad_recipe.jpg',
+                            $recipe->photo_url);
+    }
+
+    public function test_tofu_amaranth_salad_recipe() {
+        $path = "data/101cookbooks_com_tofu_amaranth_salad_cookbooks_curl.html";
+        $url  = "http://www.101cookbooks.com/archives/tofu-amaranth-salad-recipe.html";
+
+        $recipe = RecipeParser::parse(file_get_contents($path), $url);
+        if (isset($_SERVER['VERBOSE'])) print_r($recipe);
+
+        $this->assertEquals('Tofu Amaranth Salad', $recipe->title);
+
+        $this->assertEquals(10, $recipe->time['prep']);
+        $this->assertEquals(0, $recipe->time['cook']);
+        $this->assertEquals(0, $recipe->time['total']);
+        $this->assertEquals('4 servings', $recipe->yield);
+
+        $this->assertEquals(1, count($recipe->ingredients));
+        $this->assertEquals('', $recipe->ingredients[0]['name']);
+        $this->assertEquals(12, count($recipe->ingredients[0]['list']));
+
+        $this->assertEquals(1, count($recipe->instructions));
+        $this->assertEquals('', $recipe->instructions[0]['name']);
+        $this->assertEquals(1, count($recipe->instructions[0]['list']));
+
+        $this->assertEquals('http://www.101cookbooks.com/mt-static/images/food/tofu_amaranth_salad_recipe.jpg',
+                            $recipe->photo_url);
+    }
+
+}
