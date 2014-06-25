09712c06ade10ebfabe7bddf4e58de8712650f41
Mike Brittain <mike@mikebrittain.com>
2014-06-25 11:03:54 -0400

    RecipeParser_Redirect class for detecting when a clipped HTML file really contains a (canonical) link to another recipe.

---
+++ tests/RecipeParser_Redirect_Test.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once '../bootstrap.php';
+
+class RecipeParser_Redirect_Test extends PHPUnit_Framework_TestCase {
+
+    function test_inspect_for_redirect_none() {
+        $html = file_get_contents("data/bonappetit_com_chai_spiced_hot_chocolate_curl.html");
+        $url = "http://www.bonappetit.com/recipes/quick-recipes/2010/02/chai_spiced_hot_chocolate";
+        $redirect_url = RecipeParser_Redirect::inspectHtmlForRedirect($html, $url);
+        $this->assertEquals(null, $redirect_url);
+    }
+
+    function test_inspect_for_redirect_yummly() {
+        $html = file_get_contents("data/yummly_com_roasted_chicken_tacos_yummly_curl.html");
+        $url = "http://www.yummly.com/recipe/Roasted-Chicken-Tacos-Martha-Stewart-191942";
+        $redirect_url = RecipeParser_Redirect::inspectHtmlForRedirect($html, $url);
+        $this->assertEquals("http://www.yummly.com/recipe/external/Roasted-Chicken-Tacos-Martha-Stewart-191942", $redirect_url);
+    }
+
+    function test_inspect_for_redirect_yummly_iframe() {
+        $html = file_get_contents("data/yummly_com_roasted_chicken_tacos_iframe_curl.html");
+        $url = "http://www.yummly.com/recipe/external/Roasted-Chicken-Tacos-Martha-Stewart-191942";
+        $redirect_url = RecipeParser_Redirect::inspectHtmlForRedirect($html, $url);
+        $this->assertEquals("http://www.marthastewart.com/315717/roasted-chicken-tacos", $redirect_url);
+    }
+
+}
