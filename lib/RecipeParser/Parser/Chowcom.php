fd5f1c0fe6b13890fe525e3b8db9937abe4cf160
Mike Brittain <mike@mikebrittain.com>
2015-02-22 20:27:07 -0500

    Fixed Chow.com parser

--- lib/RecipeParser/Parser/Chowcom.php
+++ lib/RecipeParser/Parser/Chowcom.php
@@ -4,11 +4,45 @@ class RecipeParser_Parser_Chowcom {
 
     static public function parse($html, $url) {
         // Get all of the standard bits we can find.
-        $recipe = RecipeParser_Parser_MicrodataDataVocabulary::parse($html, $url);
+        $recipe = RecipeParser_Parser_MicrodataSchema::parse($html, $url);
+
+        // Turn off libxml errors to prevent mismatched tag warnings.
+        libxml_use_internal_errors(true);
+        $html = mb_convert_encoding($html, 'HTML-ENTITIES', "UTF-8");
+        $doc = new DOMDocument();
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $html);
+        $xpath = new DOMXPath($doc);
 
         // Titles include "recipe"
-        if (preg_match("/ Recipe$/", $recipe->title)) {
-            $recipe->title = trim(preg_replace("/(.*) Recipe$/", "$1", $recipe->title));
+        if (preg_match("/ Recipe( - CHOW.com)?$/", $recipe->title)) {
+            $recipe->title = trim(preg_replace("/(.*) Recipe( - CHOW.com)?$/", "$1", $recipe->title));
+        }
+
+        // Strip leading numbers from instructions
+        for ($i = 0; $i < count($recipe->instructions); $i++) {
+            for ($j = 0; $j < count($recipe->instructions[$i]['list']); $j++) {
+                $recipe->instructions[$i]['list'][$j] = preg_replace("/^\d+(\w.*)$/", "$1", $recipe->instructions[$i]['list'][$j]);
+            }
+        }
+
+        // Ingredients (If none parsed)
+        if (!count($recipe->ingredients[0]['list'])) {
+            $nodes = $xpath->query('//*[@id="ingredients_list"]//li');
+            foreach ($nodes as $node) {
+                $line = $node->nodeValue;
+                $line = RecipeParser_Text::formatAsOneLine($line);
+                $recipe->appendIngredient($line);
+            }
+        }
+
+        // Instructions (If none parsed)
+        if (!count($recipe->instructions[0]['list'])) {
+            $nodes = $xpath->query('//*[@itemprop="recipeInstructions"]');
+            foreach ($nodes as $node) {
+                $line = $node->nodeValue;
+                $line = RecipeParser_Text::formatAsOneLine($line);
+                $recipe->appendInstruction($line);
+            }
         }
 
         // Cleanup description
--- tests/RecipeParser_Parser_ChowcomTest.php
+++ tests/RecipeParser_Parser_ChowcomTest.php
@@ -5,7 +5,7 @@ require_once '../bootstrap.php';
 class RecipeParser_Parser_ChowcomTest extends PHPUnit_Framework_TestCase {
 
     public function test_herb_omelets() {
-        $path = "data/chow_com_herb_omelets_chow_curl.html";
+        $path = "data/chow_com_herb_omelets_chow_com_curl.html";
         $url = "http://www.chow.com/recipes/28801-herb-omelets";
 
         $recipe = RecipeParser::parse(file_get_contents($path), $url);
@@ -20,11 +20,11 @@ class RecipeParser_Parser_ChowcomTest extends PHPUnit_Framework_TestCase {
 
         $this->assertRegExp('/^These thin-as-crêpe omelets/', $recipe->description);
 
-        $this->assertStringEndsWith('28801_herbed_omlettes_2_620.jpg', $recipe->photo_url);
+        $this->assertStringEndsWith('28801_herbed_omlettes.jpg', $recipe->photo_url);
     }
 
     public function test_hungarian_chocolate_torte() {
-        $path = "data/chow_com_hungarian_chocolate_walnut_torte_chow_curl.html";
+        $path = "data/chow_com_hungarian_chocolate_walnut_torte_chow_com_curl.html";
         $url = "http://www.chow.com/recipes/29536-hungarian-chocolate-walnut-torte";
 
         $recipe = RecipeParser::parse(file_get_contents($path), $url);
@@ -36,11 +36,11 @@ class RecipeParser_Parser_ChowcomTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(12, count($recipe->ingredients[0]['list']));
         $this->assertEquals(9, count($recipe->instructions[0]['list']));
 
-        $this->assertStringEndsWith('29536_hungarian_chocolate_torte_620.jpg', $recipe->photo_url);
+        $this->assertStringEndsWith('29536_hungarian_chocolate_torte.jpg', $recipe->photo_url);
     }
 
     public function test_red_velvet_cake() {
-        $path = "data/chow_com_red_velvet_cake_chow_curl.html";
+        $path = "data/chow_com_red_velvet_cake_chow_com_curl.html";
         $url = "http://www.chow.com/recipes/29310-red-velvet-cake";
 
         $recipe = RecipeParser::parse(file_get_contents($path), $url);
@@ -52,9 +52,9 @@ class RecipeParser_Parser_ChowcomTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(17, count($recipe->ingredients[0]['list']));
         $this->assertEquals(9, count($recipe->instructions[0]['list']));
 
-        $this->assertRegExp('/This moist cocoa cake .*$/s', $recipe->description);
+        $this->assertRegExp('/This moist red velvet cake .*$/s', $recipe->description);
 
-        $this->assertStringEndsWith('29310_red_velvet_cake_3_620.jpg', $recipe->photo_url);
+        $this->assertStringEndsWith('29310_red_velvet_cake.jpg', $recipe->photo_url);
     }
 
     public function test_roasted_sardines() {
@@ -72,11 +72,11 @@ class RecipeParser_Parser_ChowcomTest extends PHPUnit_Framework_TestCase {
 
         $this->assertRegExp('/Fresh sardines are full of healthy omega-3/', $recipe->description);
 
-        $this->assertStringEndsWith('29617_roasted_sardines_potatoes_2_620.jpg', $recipe->photo_url);
+        $this->assertStringEndsWith('29617_roasted_sardines_potatoes.jpg', $recipe->photo_url);
     }
 
     public function test_white_peach_sangria() {
-        $path = "data/chow_com_white_peach_sangr_a_chow_curl.html";
+        $path = "data/chow_com_white_peach_sangr_a_chow_com_curl.html";
         $url = "http://www.chow.com/recipes/29663-white-peach-sangria";
 
         $recipe = RecipeParser::parse(file_get_contents($path), $url);
@@ -84,13 +84,13 @@ class RecipeParser_Parser_ChowcomTest extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals('White Peach Sangría', $recipe->title);
 
-        $this->assertEquals('6 servings', $recipe->yield);
+        $this->assertEquals('6 drinks', $recipe->yield);
         $this->assertEquals(7, count($recipe->ingredients[0]['list']));
         $this->assertEquals(1, count($recipe->instructions[0]['list']));
 
         $this->assertRegExp('/^.* no getting around .*$/s', $recipe->description);
 
-        $this->assertStringEndsWith('29663_white_peach_sangria_620.jpg', $recipe->photo_url);
+        $this->assertStringEndsWith('29663_white_peach_sangria.jpg', $recipe->photo_url);
     }
 
 }
