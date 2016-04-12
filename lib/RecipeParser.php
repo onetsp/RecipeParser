73ca27601a0e8f5958457ccde31470108d6985f2
OldGareBear <betheagary@gmail.com>
2016-04-12 14:21:28 -0400

    CORE-1283: fixie
    Signed-off-by: OldGareBear <betheagary@gmail.com>

--- lib/RecipeParser.php
+++ lib/RecipeParser.php
@@ -109,9 +109,9 @@ class RecipeParser {
         if (!$parser) {
             $message = '';
             if (stripos($html, "Incapsula incident") !== false) {
-                $message = 'Programmatic access to site is blocked.'
+                $message = 'Programmatic access to site is blocked.';
             }
-            throw new NoMatchingParserException();
+            throw new NoMatchingParserException($message);
         }
 
         // Initialize the right parser and run it.
