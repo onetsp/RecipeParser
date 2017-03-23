2725cc5e6c7cc194a96b80463a4ca9aff4a77723
Asaf Klibansky <discobeta@gmail.com>
2017-03-23 15:44:16 -0400

    inverting the check for the object type to check for array first and later object

--- lib/RecipeParser/Parser/MicrodataJsonLd.php
+++ lib/RecipeParser/Parser/MicrodataJsonLd.php
@@ -80,14 +80,16 @@ class RecipeParser_Parser_MicrodataJsonLd {
         // Photo
         if (property_exists($data, "image")) {
             $photo_url = $data->image;
-            if (is_object($photo_url)) {
-                $photo_url = $data->image->url;
-            }
             if (is_array($photo_url)) {
                 $first_element = array_values($photo_url)[0];
                 if ($first_element) {
-                    $photo_url = $first_element->url;
+                    if (property_exists($first_element, "url")) {
+                        $photo_url = $first_element->url;
+                    }
                 }
+            }
+            if (is_object($photo_url)) {
+                $photo_url = $data->image->url;
             }
             $recipe->photo_url = RecipeParser_Text::relativeToAbsolute($photo_url, $url);
         }
