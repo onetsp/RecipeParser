9e24a55f7aa4634c307c85fd2d00d40d3baa9114
onetsp <mike@onetsp.com>
2018-12-03 18:52:55 -0500

    Use TestUtils to get data path.

--- tests/RecipeParser_Test.php
+++ tests/RecipeParser_Test.php
@@ -5,31 +5,27 @@ require_once dirname(__FILE__) . '/../bootstrap.php';
 
 class Test extends TestCase {
 
-    private function getDataDir() {
-        return dirname(__FILE__) . "/data";
-    }
-
     function test_match_schema_markup() {
-        $html = file_get_contents($this->getDataDir() . "/schema_spec.html");
+        $html = file_get_contents(TestUtils::getDataPath("schema_spec.html"));
         $type = RecipeParser::matchMarkupFormat($html);
         $this->assertEquals(RecipeParser::SCHEMA_SPEC, $type);
 
     }
 
     function test_match_datavocabulary_markup() {
-        $html = file_get_contents($this->getDataDir() . "/datavocabulary_spec.html");
+        $html = file_get_contents(TestUtils::getDataPath("datavocabulary_spec.html"));
         $type = RecipeParser::matchMarkupFormat($html);
         $this->assertEquals(RecipeParser::DATA_VOCABULARY_SPEC, $type);
     }
 
     function test_match_microformat_markup() {
-        $html = file_get_contents($this->getDataDir() . "/microformat_spec.html");
+        $html = file_get_contents(TestUtils::getDataPath("microformat_spec.html"));
         $type = RecipeParser::matchMarkupFormat($html);
         $this->assertEquals(RecipeParser::MICROFORMAT_SPEC, $type);
     }
 
     function test_null_match_markup() {
-        $html = file_get_contents($this->getDataDir() . "/no_semantic_markup.html");
+        $html = file_get_contents(TestUtils::getDataPath("no_semantic_markup.html"));
         $type = RecipeParser::matchMarkupFormat($html);
         $this->assertEquals(null, $type);
     }
